f5fb48feff9336cb5e2af427856ca42080c25011
student.moringaschool.com <guy.nkurunziza@student.moringaschool.com>
2025-05-13 08:25:45 +0200

    add

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+    for i in range(length):
+        if i == 0:
+            sequence.append(0)
+        elif i == 1:
+            sequence.append(1)
+        else:
+            sequence.append(sequence[-1] + sequence[-2])
+    print(sequence)
